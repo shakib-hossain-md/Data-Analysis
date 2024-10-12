a199fbd2e7bbf6ede4eafe4d2c29bba50a592b32
shakib-hossain-md <sakibsanto01@gmail.com>
2024-10-12 17:01:21 +0600

    Upload

--- NumPy/Creating Arrays.py
+++ NumPy/Creating Arrays.py
@@ -35,7 +35,7 @@ print(arr)
 # 3-D array
 """An array that has 2-D arrays as its elements is called a 2-D array."""
 import numpy as np
-arr = np.array([[[1,2,3],[4,5,6],[1,2,3],[4,5,6]]])
+arr = np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
 print(arr)
 
 
