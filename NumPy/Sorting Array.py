2cd8572a8b016269da0cebe953f14125ef2e2bc0
shakib-hossain-md <sakibsanto01@gmail.com>
2024-10-17 02:50:41 +0600

    Upload

---
+++ NumPy/Sorting Array.py
@@ -0,0 +1,21 @@
+# Sort the array
+import numpy as np
+arr = np.array([3,2,0,1])
+print(np.sort(arr))
+
+# Sort the array alphabetically
+import numpy as np
+arr = np.array(['banana','cherry','apple'])
+print(np.sort(arr))
+
+# Sort a boolean array
+
+import numpy as np
+arr = np.array([True,False,True])
+print(np.sort(arr))
+
+# Sort 2D array
+
+import numpy as np
+arr = np.array([[3,2,1],[7,6,5]])
+print(np.sort(arr))
